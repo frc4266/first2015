943207f6d23cb1e514dbeda1735b500cc251c83e
millera <millera@cvcolonials.org>
2015-01-14 16:28:37 -0500

    notes to programmers

--- 2015RobotCodeV1/src/org/usfirst/frc/team4266/robot/Robot.java
+++ 2015RobotCodeV1/src/org/usfirst/frc/team4266/robot/Robot.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
+//HELLO PROGRAMMERS YAWL NJKJHKJ JKJHHKK
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
